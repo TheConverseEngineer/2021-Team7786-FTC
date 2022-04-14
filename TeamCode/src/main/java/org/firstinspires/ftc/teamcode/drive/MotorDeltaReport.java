f874de8f9ca8537294812ac237974b8b343b8b2b
Janek Vedock <janekvedock@comcast.net>
2022-04-14 09:13:13 -0400

    updated formatting in MecanumDrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MotorDeltaReport.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MotorDeltaReport.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+
 public class MotorDeltaReport {
+
     public double lf, rf, lr, rr;
 
     public MotorDeltaReport(double lf, double rf, double lr, double rr){
