def0ecb247fa5d5005bd9eb1bd497e2c30e79a52
Janek Vedock <janekvedock@comcast.net>
2022-04-14 09:17:58 -0400

    updated formatting in MecanumKinematics

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kinematics/MecanumKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kinematics/MecanumKinematics.java
@@ -6,6 +6,7 @@ import org.firstinspires.ftc.teamcode.drive.MotorDeltaReport;
 import org.firstinspires.ftc.teamcode.utils.Pose2D;
 import org.firstinspires.ftc.teamcode.utils.Vector2D;
 
+
 public class MecanumKinematics {
 
     private static MecanumKinematics instance = new MecanumKinematics();
