e5bfd60e535828d6190067abe986d8c67e65fb79
Janek Vedock <janekvedock@comcast.net>
2022-04-14 09:53:16 -0400

    updated formatting in TrajectoryState

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectory/TrajectoryState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectory/TrajectoryState.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.trajectory;
 
 import org.firstinspires.ftc.teamcode.utils.Pose2D;
 
+
 public class TrajectoryState {
     public Pose2D target;
     public double velocity;
