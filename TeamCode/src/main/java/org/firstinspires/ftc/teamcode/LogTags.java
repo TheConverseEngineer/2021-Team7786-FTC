d32a7857168e3baa71c8a400c5a32a7b119d8fbc
Janek Vedock <janekvedock@comcast.net>
2022-04-20 09:52:27 -0400

    added logging to fourwheeldrive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LogTags.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LogTags.java
@@ -2,4 +2,5 @@ package org.firstinspires.ftc.teamcode;
 
 public class LogTags {
     public static final String HARDWARE_TAG = "HARDWARE";
+    public static final String DRIVE_TAG = "DRIVE";
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FourWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/FourWheelDrive.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.drive;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.LogTags;
 import org.firstinspires.ftc.teamcode.hardware.DriveMotor;
 import org.firstinspires.ftc.teamcode.kinematics.ChassisCommand;
 
@@ -17,6 +20,7 @@ public class FourWheelDrive {
      * @param rightRearDrive        right rear motor
      */
     public FourWheelDrive(DriveMotor leftFrontDrive, DriveMotor rightFrontDrive, DriveMotor leftRearDrive, DriveMotor rightRearDrive) {
+        Log.i(LogTags.DRIVE_TAG, "Four Wheel Drive being instantiated");
         this.leftFrontDrive = leftFrontDrive;
         this.rightFrontDrive = rightFrontDrive;
         this.leftRearDrive = leftRearDrive;
@@ -26,9 +30,11 @@ public class FourWheelDrive {
         this.rightFrontDrive.getMotor().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         this.leftFrontDrive.getMotor().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         this.rightRearDrive.getMotor().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Log.i(LogTags.DRIVE_TAG, "Four Wheel Drive instantiated");
     }
 
     public void start() {
+        Log.v(LogTags.DRIVE_TAG, "Four Wheel Drive being started");
         this.leftFrontDrive.getMotor().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         this.rightFrontDrive.getMotor().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         this.leftRearDrive.getMotor().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -36,6 +42,7 @@ public class FourWheelDrive {
     }
 
     public void runCommand(ChassisCommand cmd) {
+        Log.i(LogTags.DRIVE_TAG, "Four Wheel Drive command running");
         leftFrontDrive.setVelocity(cmd.getLF());
         rightFrontDrive.setVelocity(cmd.getRF());
         leftRearDrive.setVelocity(cmd.getLR());
