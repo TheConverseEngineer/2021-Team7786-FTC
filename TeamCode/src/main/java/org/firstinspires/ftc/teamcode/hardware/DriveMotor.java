bef26670d3577c581c1f6c0bbb72da4b2e38689b
Janek Vedock <janekvedock@comcast.net>
2022-04-20 09:43:39 -0400

    added logging to DriveMotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveMotor.java
@@ -4,7 +4,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import static org.firstinspires.ftc.teamcode.LogTags.*;
 import static org.firstinspires.ftc.teamcode.tests.ROBOT_DATA.*;
+
+import android.util.Log;
 
 
 public class DriveMotor {
@@ -13,14 +16,18 @@ public class DriveMotor {
 
     // Stored as encoder ticks
     private int lastPos;
+    private String id;
 
     public DriveMotor(HardwareMap hwMap, String id, DcMotor.Direction direction) {
+        Log.i(HARDWARE_TAG, "DriveMotor with id " + id + " being instantiated");
+        this.id = id;
         this.motor = hwMap.get(DcMotorEx.class, id);
         this.motor.setDirection(direction);
         this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         this.motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         lastPos = 0;
+        Log.i(HARDWARE_TAG, "DriveMotor with id " + id + " instantiated");
     }
 
     public DcMotorEx getMotor() {
@@ -29,6 +36,7 @@ public class DriveMotor {
 
     public void setPower(double power) {
         this.motor.setPower(power);
+        Log.v(HARDWARE_TAG, "DriveMotor " + this.id + " power set to " + power);
     }
 
     /** Returns the change in position since last call
@@ -46,5 +54,6 @@ public class DriveMotor {
      */
     public void setVelocity(double velocity) {
         this.motor.setVelocity(velocity * TICKS_PER_REV);
+        Log.v(HARDWARE_TAG, "DriveMotor " + this.id + " velocity set to " + velocity);
     }
 }
