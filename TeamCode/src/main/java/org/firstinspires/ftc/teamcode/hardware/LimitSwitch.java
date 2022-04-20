c4299389b8df5015c9e88c9991b3dd8f37063b6c
Janek Vedock <janekvedock@comcast.net>
2022-04-20 09:48:36 -0400

    added logging to limitswitch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/LimitSwitch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/LimitSwitch.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import static org.firstinspires.ftc.teamcode.LogTags.HARDWARE_TAG;
+
+import android.util.Log;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
@@ -10,7 +14,9 @@ public class LimitSwitch {
     private boolean isPressed;
 
     public LimitSwitch(HardwareMap hwMap, String id) {
+        Log.i(HARDWARE_TAG, "LimitSwitch with id " + id + " being instantiated");
         limitSwitch = hwMap.get(TouchSensor.class, id);
+        Log.i(HARDWARE_TAG, "LimitSwitch with id " + id + " instantiated");
     }
 
     public boolean isActivated() {
